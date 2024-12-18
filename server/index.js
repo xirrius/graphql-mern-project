a762d3fce18270c71544b35da588044d3eb562a2
Shreya Nigam <shreya.nigam1062@gmail.com>
2024-12-18 09:39:47 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+require("dotenv").config();
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
+const colors = require("colors");
+const connectDB = require("./config/db");
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === "development",
+  })
+);
+connectDB();
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
